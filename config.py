313f42f67c8d0a9d393dbacd94879d92d456178c
RitsamHub <ritsam7890@gmail.com>
2026-04-14 21:02:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,8 +15,8 @@ import os
 # ==============================
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", "0"))
-API_HASH = os.environ.get("API_HASH", "")
+API_ID = int(os.environ.get("API_ID", "29296297"))
+API_HASH = os.environ.get("API_HASH", "477a9529cfd84f4088539c6ab94351fc")
 
 
 # ==============================
@@ -24,14 +24,14 @@ API_HASH = os.environ.get("API_HASH", "")
 # ==============================
 
 # Add admin user IDs separated by commas in environment variables
-ADMINS = [int(admin) for admin in os.environ.get("ADMINS", "").split(",") if admin]
+ADMINS = [int(admin) for admin in os.environ.get("ADMINS", "5094080507").split("5094080507,1981108681") if admin]
 
 
 # ==============================
 # Database Configuration
 # ==============================
 
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://Raman:RamanOp@cluster0.faapcwf.mongodb.net/")
 DB_NAME = os.environ.get("DB_NAME", "SaveRestricted2")
 
 
@@ -40,7 +40,7 @@ DB_NAME = os.environ.get("DB_NAME", "SaveRestricted2")
 # ==============================
 
 # Replace with your Telegram log channel ID (example: -1001234567890)
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003992468621"))
 
 
 # ==============================
